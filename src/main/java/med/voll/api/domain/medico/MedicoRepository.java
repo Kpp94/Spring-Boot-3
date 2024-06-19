a8cec9f110952483ae895a059ac519c6596c36aa
Kevin <kpp_9405@hotmail.com>
2024-06-18 21:40:59 -0600

    commit from intellij

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable paginacion);
 
+    //Primer cambio en intellij
+
     @Query("""
             select m from Medico m
             where m.activo= 1 and
